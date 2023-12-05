ceae354710af93eae0b59e384007ec914d9ac342
Ivo van der Zeyst <ivo.van.der.zeyst@pon.com>
2023-12-06 00:10:40 +0100

    added start and end tile (hardcoded for now), changed grid loop to be more dynamic and seperated the grid expansion from the loop itterators

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -11,6 +11,7 @@ public class Background : MonoBehaviour
     private int XScale = 5;
     private int YScale = 1;
     private int ZScale = 5;
+    private int GridDimension = 5;
     
     // Start is called before the first frame update
     void Start()
@@ -18,28 +19,35 @@ public class Background : MonoBehaviour
         // create background
         Debug.Log("Started creating background");
         GameObject Background = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Background.transform.localScale = new Vector3(1000, 0, 1000);
-        Background.transform.position = new Vector3(12, -1, 12);
+        Background.transform.localScale = new Vector3(1000, 0, 1000); //TODO: remove hardcode later
+        Background.transform.position = new Vector3(12, -1, 12); //TODO: remove hardcode later
         
         // create play grid
         Debug.Log("Started creating grid");
-        for (int i = 0; i <= 24; i = i + 6) 
+        var XIncrease = 0;
+       
+        for (int i = 0; i < GridDimension; i++)
         {
-            for (int j = 0; j <= 24; j = j + 6)
+            var ZIncrease = 0;
+            for (int j = 0; j < GridDimension; j++)
             {
                 GameObject Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 Cube.transform.localScale = new Vector3(XScale, YScale, ZScale);
-                Cube.transform.position = new Vector3(XPosition + i, YPosition, ZPosition + j);
+                Cube.transform.position = new Vector3(XPosition + XIncrease, YPosition, ZPosition + ZIncrease);
 
-                Cube.name = i.ToString() + '-' + j.ToString();
+                Cube.name = i.ToString() + "-" + j.ToString();
+                ZIncrease = ZIncrease + 6;
             }
+            XIncrease = XIncrease + 6;
         }
         
+        // create start tile, TODO, make dynamicly insteasd of hardcoded
+        var start_tile = GameObject.Find("2-0");
+        start_tile.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
         
-        
-        
-        
-        
+        // create end tile, TODO, make dynamicly insteasd of hardcoded
+        var end_tile = GameObject.Find("2-4");
+        end_tile.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
     }
     
     
